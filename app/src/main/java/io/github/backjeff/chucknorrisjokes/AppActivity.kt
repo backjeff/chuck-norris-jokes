e2cb669584e247769a50c15aaaf147ab3a5f3cdb
jefferson <jefferson@ciandt.com>
2020-03-23 14:48:26 -0300

    Feature random_joke_screen: Fix navigation to previous fragment

--- app/src/main/java/io/github/backjeff/chucknorrisjokes/AppActivity.kt
+++ app/src/main/java/io/github/backjeff/chucknorrisjokes/AppActivity.kt
@@ -2,12 +2,18 @@ package io.github.backjeff.chucknorrisjokes
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import io.github.backjeff.chucknorrisjokes.intent.utils.safeNavigateUp
 
 class AppActivity : AppCompatActivity() {
+
+    private val navController by lazy { findNavController(R.id.baseContainer) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_app)
     }
+
+    override fun onSupportNavigateUp() = navController.safeNavigateUp()
 
 }
